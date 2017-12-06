a58642f279f902bbfe7e2617758e3f71b807b588
Dillon <dcblood1@gmail.com>
2017-12-06 09:42:12 -0600

    Working adapter
    have working RecyclerViewAdapter in Main activity. Woo!

--- app/src/main/java/com/example/android/aarv1/MainActivity.java
+++ app/src/main/java/com/example/android/aarv1/MainActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.android.aarv1.adapter.AARHolder;
+import com.example.android.aarv1.model.AAR;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
@@ -17,7 +25,12 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     // Access a Cloud Firestore instance from your Activity
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -27,37 +40,55 @@ public class MainActivity extends AppCompatActivity {
     // Document Reference grabs a single document
     DocumentReference docRef = db.collection("aars").document("bU2QmGtG6hCTfpkUJB5I");
 
+    // query the firestore db
+    Query query = db
+            .collection("aars")
+    .orderBy("timeStamp", Query.Direction.DESCENDING)
+    .limit(50);
+
+    Query query2 = db.collection("aars").orderBy("timeStamp", Query.Direction.ASCENDING);
+
+    @BindView(R.id.recycler_aars)
+    RecyclerView mAarsRecycler;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // dont know what this does...
+        ButterKnife.bind(this);
+
+        mAarsRecycler.setHasFixedSize(true);
+        mAarsRecycler.setLayoutManager(new LinearLayoutManager(this));
+
 
         // Set up FAB to open editorActivity (incoming)
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent editIntent = new Intent(MainActivity.this,EditorActivity.class);
+                Intent editIntent = new Intent(MainActivity.this, EditorActivity.class);
                 startActivity(editIntent);
             }
         });
 
         // Just try to pull data any way that you can.
         // I need to grab data and turn it back into a custom class...
-        Log.v("MainActivity.java","collectionRef = " + collectionRef);
+        Log.v("MainActivity.java", "collectionRef = " + collectionRef);
 
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()){
+                if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document != null) {
-                        Log.v("MainActivity.java","document: " + task.getResult().getData());
+                        Log.v("MainActivity.java", "document: " + document.exists());
                     } else {
-                        Log.v("MainActivity.java","no document exists");
+                        Log.v("MainActivity.java", "no document exists");
                     }
                 } else {
-                    Log.v("MainActivity.java","get failed with: ", task.getException());
+                    Log.v("MainActivity.java", "get failed with: ", task.getException());
                 }
             }
         });
@@ -68,22 +99,50 @@ public class MainActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
                     for (DocumentSnapshot document : task.getResult()) {
-                        Log.v("MainActivity.java",document.getId() + " => " + document.getData());
+                        Log.v("MainActivity.java", document.getId() + " => " + document.getData());
                     }
                 } else {
-                    Log.v("MainActivity.java","Error getting documents", task.getException());
+                    Log.v("MainActivity.java", "Error getting documents", task.getException());
                 }
             }
         });
 
-
-        // query the firestore db
-        Query query = db
-                .collection("aars")
-                .orderBy("timestamp")
-                .limit(50);
+    };// end onCreate()
 
 
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        attachRecyclerViewAdapter();
     }
+
+    private void attachRecyclerViewAdapter() {
+        final RecyclerView.Adapter adapter = newAdapter();
+        mAarsRecycler.setAdapter(adapter);
+    }
+
+    protected RecyclerView.Adapter newAdapter() {
+        FirestoreRecyclerOptions<AAR> options =
+                new FirestoreRecyclerOptions.Builder<AAR>()
+                        .setQuery(query, AAR.class)
+                        .setLifecycleOwner(this)
+                        .build();
+
+        Log.v(TAG,"this is the query: " + query);
+
+        return new FirestoreRecyclerAdapter<AAR, AARHolder>(options) {
+            @Override
+            public AARHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return new AARHolder(LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.aar_item, parent, false));
+            }
+
+            @Override
+            protected void onBindViewHolder(AARHolder holder, int position, AAR model) {
+                holder.bind(model);
+            }
+        };
+    }
+
 }
+
---
+++ app/src/main/java/com/example/android/aarv1/adapter/AARHolder.java
@@ -0,0 +1,72 @@
+package com.example.android.aarv1.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.android.aarv1.R;
+import com.example.android.aarv1.model.AAR;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Dillon on 12/5/2017.
+ * RecyclerView Holder
+ */
+
+public class AARHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.aar_item_image)
+    ImageView imageView;
+
+    @BindView(R.id.aar_title)
+    TextView titleTextView;
+
+    @BindView(R.id.aar_category_name)
+    TextView categoryTextView;
+
+    @BindView(R.id.aar_recommendations)
+    TextView recommendationsTextView;
+
+    @BindView(R.id.up_votes)
+    TextView upVotesTextView;
+
+    @BindView(R.id.down_votes)
+    TextView downVotesTextView;
+
+    @BindView(R.id.up_vote_button)
+    Button upVoteButton;
+
+    @BindView(R.id.down_vote_button)
+    Button downVoteButton;
+
+    public AARHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    // don't know if this is the correct thing to do... using AAR... but we'll dam sure try
+    // dont think I need to set the button on here... but not sure honestly
+    public void bind (AAR aar){
+
+        titleTextView.setText(aar.getTitle());
+        categoryTextView.setText(aar.getCategory());
+        recommendationsTextView.setText(aar.getRecommendations());
+
+
+// this doesn't work right now becuase it is trying to set text with an int
+//        upVotesTextView.setText(aar.getUpVotes());
+//        downVotesTextView.setText(aar.getDownVotes());
+
+        // Load aar image into imageView
+        Glide.with(imageView.getContext())
+                .load(aar.getPhoto())
+                .into(imageView);
+
+
+    }
+}
